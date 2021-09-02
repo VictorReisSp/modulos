f3e74ad254ecdc58b199ff4d11dac922e462b1b3
VictorReisSp <31421139+VictorReisSp@users.noreply.github.com>
2021-09-02 18:54:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+import {conteudoModulo, umaSominhaAi} from 'modulo.mjs';
+console.log(conteudoModulo)
+
+
---
+++ modulo.mjs
@@ -0,0 +1,11 @@
+export {conteudoModulo, umaSominhaAi};
+
+const conteudoModulo = {
+    'nome' : 'Victor Reis',
+    'idade' : 21,
+    'prazeres' : 'console sem nenhum erro',
+    'desprazeres' : 'CORS',
+
+}
+
+const umaSominhaAi = 2+2;
